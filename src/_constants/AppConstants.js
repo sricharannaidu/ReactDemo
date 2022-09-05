c51a59b748e94ebf895844e8504f983fc89cec77
sricharannaidu <sricharannaidu@gmail.com>
2022-09-05 18:21:25 -0400

    Initial commit

---
+++ src/_constants/AppConstants.js
@@ -0,0 +1,8 @@
+// Used initially to update the displayed version number
+// FIXED_TAB_COUNT represents the number of fixed tabs to left of tab area (before dynamic tabs for cases)
+export const appConstants = {
+    APPNAME: "Estimation Tool",
+    FIXED_TAB_COUNT: 1,
+    SHOW_WORKGROUP_BASKETS: false,
+    DEFAULT_RIGHT_PANEL_SECTION: "pyCaseDetails"
+};
